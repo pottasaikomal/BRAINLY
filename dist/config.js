c0310f2453e4e20b7437a8bcaa95616f9b060a70
Sai Komal Potta <komal@Sais-MacBook-Air.local>
2024-12-03 01:39:20 +0530

    Remove ignored files from tracking

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_URL = exports.JWTPASSKEY = void 0;
-exports.JWTPASSKEY = "123456";
-exports.DB_URL = "mongodb+srv://20cs01002:5AlJFtVk8QKD13pe@projects.t9x2v.mongodb.net/Brainly";
